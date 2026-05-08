6b0b72a161b680b6f1d2d791188d283f8e70b204
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-05-08 11:02:45 +0200

    Ejercicio 14 del bloque 5 de programacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,51 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Formulario Telefono");
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        Label telefonoLabel = new Label("Telefono");
+        TextField telefonoTextField = new TextField();
+        Button botonAceptar = new Button("Aceptar");
+        botonAceptar.setOnAction(e -> {
+            String telefono = telefonoTextField.getText();
+            if(telefono.matches("\\d{9}")){
+                Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+                alerta.setTitle("Informacion");
+                alerta.setHeaderText("Telefono ingresado: " + telefono);
+                alerta.showAndWait();
+            }else{
+                Alert alerta = new Alert(Alert.AlertType.WARNING);
+                alerta.setTitle("Error");
+                alerta.setHeaderText("Error de telefono");
+                alerta.showAndWait();
+            }
+        });
+        gridPane.add(telefonoLabel, 0, 0);
+        gridPane.add(telefonoTextField, 1, 0);
+        gridPane.add(botonAceptar, 1, 1);
+
+        Scene scene = new Scene(gridPane, 300, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
